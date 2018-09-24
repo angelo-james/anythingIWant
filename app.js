52e449d64ef16bd8f8f8b3073c2bd248c7888396
seintun <seintun.edu@gmail.com>
2018-09-24 16:33:14 -0700

    Gurlabe added and fixed CORS

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+//method with 1 argument hat passes 3 arguments to the callback
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    //the * allows anyone to connect to this API 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get('/fruits', (req, res)=>{
     res.send(`Hello g101!, I am a basket of fruits!`)
